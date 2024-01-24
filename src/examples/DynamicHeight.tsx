2a9371478dcabec0b101a3832840c4792859312e
Salman <salman@beelinerouter.net>
2024-01-24 23:58:39 +0300

    set scroll event

--- src/examples/DynamicHeight.tsx
+++ src/examples/DynamicHeight.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { createRef, useEffect, useLayoutEffect, useRef, useState } from "react";
 
 const items = Array.from({ length: 1_000 }, (_, index) => ({
   id: Math.random().toString(36).slice(2),
@@ -10,6 +10,26 @@ const containerHeight = 800;
 
 export const DynamicHeight = () => {
   const [listItems, setListItems] = useState(items)
+  const [scrollTop, setScrollTop] = useState(0)
+  const scrollElementRef = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    const scrollElement = scrollElementRef.current
+
+    if(!scrollElement) return
+
+    const handleScroll = () => {
+      const scrollTop = scrollElement.scrollTop
+
+      setScrollTop(scrollTop)
+    }
+
+    scrollElement.addEventListener('scroll', handleScroll)
+
+    return () => scrollElement.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  console.log(scrollTop)
 
   return (
     <div style={{ padding: 12 }}>
@@ -27,6 +47,7 @@ export const DynamicHeight = () => {
           overflow: 'auto',
           border: '1px solid lightgrey'
         }}
+        ref={scrollElementRef}
       >
         {listItems.map((item) => {
           return (
